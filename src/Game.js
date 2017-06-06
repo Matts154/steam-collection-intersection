4dc137f610f33332cd5896a612ee726a8ff5bd14
Matt <bored154@gmail.com>
2017-06-05 20:20:08 -0700

    Replaced String.format() with ES6 string templating.

--- src/Game.js
+++ src/Game.js
@@ -6,10 +6,10 @@ class Game extends Component {
 		const { appid, name } = this.props.game;
 		// const appid = this.props.game.appid;
 		// const name = this.props.game.name;
-		const href = this.props.game.img_logo_url ? "http://media.steampowered.com/steamcommunity/public/images/apps/{0}/{1}.jpg".format(appid, this.props.game.img_logo_url) : "./img/default.png";
+		const href = this.props.game.img_logo_url ? `http://media.steampowered.com/steamcommunity/public/images/apps/${appid}/${this.props.game.img_logo_url}.jpg` : "./img/default.png";
 		return (
 			<div id={appid} className="col-md-6">
-				<a className="game" href={"steam://run/{0}".format(appid)}>
+				<a className="game" href={`steam://run/${appid}`}>
 					<div className="game-image">
 						<img src={href} alt={name} className=""/>
 					</div>
