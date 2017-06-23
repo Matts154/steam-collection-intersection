cc8b340ec97612f149245a8373a544246120bc33
Matt <bored154@gmail.com>
2017-06-22 23:15:09 -0700

    Initial commit of Pagination component

---
+++ src/Pagination.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+class Pagination extends Component {
+	render() {
+		const num = this.props.num;
+
+		return (
+			<div>
+				<div>&nbsp;1&nbsp;</div>
+				<div>&nbsp;2&nbsp;</div>
+			</div>
+		);
+	}
+}
+
+export default Pagination;
