cf6c140786b1d894698c4db38251e5d9313b4603
Matt <bored154@gmail.com>
2017-06-23 01:05:43 -0700

    Implemented Pagination component for games

--- src/GameList.js
+++ src/GameList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 
 import Game from "./Game";
+import Pagination from "./Pagination";
 
 class GameList extends Component {
 	constructor() {
@@ -26,9 +26,9 @@ class GameList extends Component {
 		return (
 			<div id="games-list">
 				<p>Number of games: {this.props.games.length}</p>
-				<div id="games">
+				<Pagination id="games">
 					{gameComponents.slice((this.state.page-1) * this.state.pageSize, this.state.page * this.state.pageSize)}
-				</div>
+				</Pagination>
 			</div>
 		);
 	}
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 
 class Pagination extends Component {
+	constructor() {
+		super();
+		this.state = {
+			page: 1,
+			numPerPage: 14,
+		};
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.children !== undefined;
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		if (nextProps.children.length != this.props.children.length) {
+			this.setState(
+				Object.assign({}, this.state, {page: 1})
+			);
+		}
+	}
+
+	changePage(page) {
+		this.setState(Object.assign({}, this.state, {page}));
+	}
+
 	render() {
-		const num = this.props.num;
+		const { page, numPerPage } = this.state;
+		const num = this.props.children.length;
+		const pageNumbers = Array(Math.ceil(num / numPerPage)).fill(null).map((_, index) => {
+			const page = index + 1;
+			return (<a className="page-number" value={page} onClick={() => this.setState(Object.assign({}, this.state, {page}))}>{page}</a>);
+		});
 
 		return (
-			<div>
-				<div>&nbsp;1&nbsp;</div>
-				<div>&nbsp;2&nbsp;</div>
+			<div className="pagination">
+				<div className="page-number-list">
+					{pageNumbers}
+				</div>
+				<div {...this.props}>
+					{this.props.children.slice((page - 1) * numPerPage, page * numPerPage)}
+				</div>
 			</div>
 		);
 	}
