a68ba4387804cc12ed959afbd30ee533129260ce
Matt <bored154@gmail.com>
2017-07-22 23:17:23 -0700

    Added the addUserBySteamURL
    It'll now dispatch different actions depending on what the steam identifier is (username/steamid)

--- src/action/UserActions.js
+++ src/action/UserActions.js
@@ -1,2 +1,24 @@
 import { dispatcher } from "../dispatcher.js";
-import Constants from "../constant/Constants";
+import { USER } from "../constant/ActionConstants";
+
+// Example Vanity URL: http://steamcommunity.com/id/bored154/
+// Example Normal URL: http://steamcommunity.com/profiles/76561197977769301/
+function addUserBySteamURL(steamIdentifier) {
+	if (steamIdentifier.includes("://steamcommunity.com")) {
+		var re = /(id|profile)\/(.+)\/$/
+		steamIdentifier = re.exec(steamIdentifier)[2]
+	}
+
+	if(Number.parseInteger(steamIdentifier)) {
+		dispatcher.dispatch({
+			type: USER.ADD_BY.ID,
+			data: steamIdentifier
+		});
+	} else {
+		dispatcher.dispatch({
+			type: USER.ADD_BY.VANITY,
+			data: steamIdentifier
+		});
+	}
+
+}
