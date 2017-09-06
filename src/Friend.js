c6a08e563edb8571ed25bd10793d5ff970e7f02f
Matt <bored154@gmail.com>
2017-09-06 00:33:59 -0700

    Removed unused code and made the selected state bound to the App's state

--- src/Friend.js
+++ src/Friend.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 
 class Friend extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			checked: false
-		}
-	}
-
 	toggle(event) {
 		const { steamid } = this.props.friend;
-		const checked = !this.state.checked;
-		this.setState(
-			Object.assign(this.state, {
-				checked
-			})
-		)
+		const selected = this.props.selected;
 
-		this.props.handleClick(steamid, checked);
+		this.props.handleClick(steamid, !selected);
 	}
 
 	render() {
 		const { steamid, avatarfull, personaname } = this.props.friend;
-		const { checked } = this.state;
+		const selected = this.props.selected;
 
 		return (
-			<img id={steamid} className={"friend" + (checked ? " checked" : "")} src={avatarfull} alt={personaname} title={personaname} onClick={this.toggle.bind(this)} />
+			<div id={steamid} className={"friend" + (selected ? " checked" : "")} onClick={this.toggle.bind(this)}>
+				<img src={avatarfull} alt={personaname} title={personaname} />
+			</div>
 		);
 	}
 }
