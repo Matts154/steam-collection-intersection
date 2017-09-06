b22b085dc31878429fdfdea7424bf70002b62b89
Matt <bored154@gmail.com>
2017-09-06 00:35:57 -0700

    Cleaned up code a little

--- src/GameList.js
+++ src/GameList.js
@@ -17,7 +17,9 @@ class GameList extends Component {
 	}
 
 	render() {
-		if (!this.props.games) { return (<div></div>); }
+		if (!this.props.games || this.props.games.length === 0) {
+			 return null;
+		 }
 
 		let gameComponents = this.props.games.map((game, i) => {
 			return (<Game key={game.appid} game={game}/>);
