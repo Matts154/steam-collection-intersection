993de23b8c0bd9369a64193b4512759f38b95865
Matt <bored154@gmail.com>
2017-09-06 23:31:22 -0700

    Removed unused code from GamesList

--- src/pages/SteamCollectionIntersection/components/GameList/index.js
+++ src/pages/SteamCollectionIntersection/components/GameList/index.js
@@ -4,18 +4,6 @@ import Game from "../Game";
 import Pagination from "../Pagination";
 
 class GameList extends Component {
-	constructor() {
-		super();
-		this.state = {
-			pageSize: 3000,
-			page: 1,
-		}
-	}
-
-	handleClick(event){
-
-	}
-
 	render() {
 		if (!this.props.games || this.props.games.length === 0) {
 			 return null;
@@ -29,7 +17,7 @@ class GameList extends Component {
 			<div id="games-list">
 				<p className="num-games">Number of games: {this.props.games.length}</p>
 				<Pagination id="games">
-					{gameComponents.slice((this.state.page-1) * this.state.pageSize, this.state.page * this.state.pageSize)}
+					{gameComponents}
 				</Pagination>
 			</div>
 		);
